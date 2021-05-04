848d7ae603bef63716bf7c2598d85db6bd29352b
Atul <atuljadhav618@gmail.com>
2021-05-04 22:01:34 +0530

    Final commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Placement Portal</h1>
+      <p data-ns-test="project-description">Simple web application</p>
+    </div>
   )
 }
 
